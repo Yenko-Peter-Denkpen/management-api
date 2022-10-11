1bc5f65b29dac14f03f7d4a7af6c4258087db417
yenko peter denkpen <jamilaal865@gmail.com>
2022-10-11 12:52:10 +0000

    odfpkoeskd

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const dotenv=require("dotenv");
 const connectDB =require("./config/connectDB");
 const userRoute = require("./routes/userRoute");
 const RentRoute = require("./routes/RentRoute")
-const morgan = require("morgan");;
+const morgan = require("morgan");
+const cors =require("cors")
 
 const app=express();
 dotenv.config();
 connectDB();
 
 // middlewares
+app.use(cors())
 app.use(express.json())
 app.use("/api/users", userRoute)
 app.use(morgan("dev"));
